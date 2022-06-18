d0ca89fed3b5391924615b063d77b1b106705a52
Katopan <76249494+katomizuki@users.noreply.github.com>
2022-06-19 08:56:04 +0900

    RealityComposer Complete

--- AR-Begginer/RealityComposer/RealityComposerView.swift
+++ AR-Begginer/RealityComposer/RealityComposerView.swift
@@ -6,11 +6,26 @@
 //
 
 import SwiftUI
+import RealityKit
+import ARKit
 
 struct RealityComposerView: View {
     var body: some View {
-        VStack {
-            
-        }
+        RealityComposerViewContainer().edgesIgnoringSafeArea(.all)
     }
+}
+
+struct RealityComposerViewContainer: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> ARView {
+        let arView = ARView(frame: .zero)
+        let ballon = try! Experience.loadBallon()
+        arView.scene.anchors.append(ballon)
+        return arView
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {
+        
+    }
+    
 }
